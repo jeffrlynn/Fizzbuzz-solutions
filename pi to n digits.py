a9654bf5db43e269dd2b35972655905dffb4f274
Jeffrey Lynn <jeffreyrlynn@gmail.com>
2018-01-12 21:56:56 -0500

    add pi maker

---
+++ pi to n digits.py
@@ -0,0 +1,15 @@
+#Program will prompt user to input the number of pi decmimals they would like to see, then give them the result.
+
+
+number = int(input("How many decimals of pi would you like to see?\nEnter and number from 0 to 50:  "));
+
+from math import pi
+def CalcPi(number):
+    if number == 0:
+        return 3
+    if 0 < number < 50:
+        return format(pi, '.' + str(number) + 'f');
+    else:
+        return "Too many digits requested.";
+
+print (CalcPi(number));
