500486912ea9aa6d3995190d17df7c86f6922a8a
Jeffrey Lynn <jeffreyrlynn@gmail.com>
2018-01-13 00:22:37 -0500

    add e power

---
+++ e to n power.py
@@ -0,0 +1,15 @@
+#Program will prompt user to input the the power they would like e raised to, then give them the result.
+
+
+number = int(input("What power of e would you like to see?\nEnter any number from 0 to 50:  "));
+
+from math import e
+def CalcE(number):
+    if number == 0:
+        return 1.0
+    if 0 < number < 50:
+        return e**number;
+    else:
+        return "Too powerful!";
+
+print (CalcE(number));
